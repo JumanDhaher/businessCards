d124c3ed57a489f0cca5490de2a142262795d6c1
Juman <juman.developer@gmail.com>
2023-12-18 11:41:20 +0300

    Initial Commit

---
+++ businessCards/businessCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  businessCardsApp.swift
+//  businessCards
+//
+//  Created by Juman Dhaher on 05/06/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct businessCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
