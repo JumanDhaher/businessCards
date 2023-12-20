3168ddfea9d675b9781560db1564c8291c5d85e2
shomokh123 <ip5115@outlook.sa>
2023-12-20 12:54:57 +0300

    change1

---
+++ cardColorPage.swift
@@ -0,0 +1,18 @@
+//
+//  cardColorPage.swift
+//  businessCards
+//
+//  Created by shomokh aldosari on 07/06/1445 AH.
+//
+
+import SwiftUI
+
+struct cardColorPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    cardColorPage()
+}
