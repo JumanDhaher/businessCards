61ea20c5196e979cbd6cd37d55cf651dae187ec8
shomokh123 <ip5115@outlook.sa>
2023-12-25 13:40:41 +0300

    card scroll

---
+++ businessCards/Home.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  businessCards
+//
+//  Created by shomokh aldosari on 12/06/1445 AH.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Home()
+}
