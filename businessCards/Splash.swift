7b14082b3acdfda449036107137a1ca5d35c06ae
shomokh123 <ip5115@outlook.sa>
2023-12-26 09:26:06 +0300

    splash

---
+++ businessCards/Splash.swift
@@ -0,0 +1,18 @@
+//
+//  Splash.swift
+//  businessCards
+//
+//  Created by shomokh aldosari on 13/06/1445 AH.
+//
+
+import SwiftUI
+
+struct Splash: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Splash()
+}
